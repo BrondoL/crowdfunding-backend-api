301b033a0eeefa546bb58883afa7c32646aa0d8b
Aulia Ahmad Nabil <nabilunited2@gmail.com>
2022-04-23 18:53:46 +0700

    Endpoint GetCampaigns (WIP)

--- campaign/service.go
+++ campaign/service.go
@@ -1,1 +1,29 @@
 package campaign
+
+type Service interface {
+	GetCampaigns(id int) ([]Campaign, error)
+}
+
+type service struct {
+	repository Repository
+}
+
+func NewService(repository Repository) *service {
+	return &service{repository}
+}
+
+func (s *service) GetCampaigns(id int) ([]Campaign, error) {
+	var campaigns []Campaign
+	var err error
+
+	if id != 0 {
+		campaigns, err = s.repository.FindByUserId(id)
+	} else {
+		campaigns, err = s.repository.FindAll()
+	}
+
+	if err != nil {
+		return campaigns, err
+	}
+	return campaigns, nil
+}
